a4fae612da3cd568c66caa00eb2d492e72eabe40
Mauli96k <amargavhane402@gmail.com>
2025-06-24 15:05:08 +0530

    Amar first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "./styles/pages.css";
+import Header from './componets/Headers/Header'
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import AdmissionsPage from "./pages/AdmissionsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+
+
+const App = () =>{
+  return(
+    <Router>
+      <div className="main-layout">
+
+<Header/>
+      
+  <Routes>
+           <Route path="/" element={<HomePage />} />
+           <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/admissions" element={<AdmissionsPage />} />
+            
+            <Route path="*" element={<NotFoundPage />} />
+           
+
+        </Routes>
+      </div>
+
+      
+
+
+    </Router>
+  )
+}
+export default App;
+
